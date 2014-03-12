bbda40011ca261942ebc5ef8bf1e6709ba6b249f
Morgan Bird <morgan.bird@gmail.com>
2014-03-12 09:11:35 -0500

    Fixed syntax error.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,7 @@ TascPortal::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  config.action_mailer.default_url_options = { :host => 'tasc.morganbird.com' 
+  config.action_mailer.default_url_options = { :host => 'tasc.morganbird.com' } 
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
