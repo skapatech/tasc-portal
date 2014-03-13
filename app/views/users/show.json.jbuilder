13e445ff8a7bb972d662495c35adbd398a23c518
Morgan Bird <morgan.bird@gmail.com>
2014-03-12 22:22:25 -0500

    Started user management.

---
+++ app/views/users/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @user, :id, :email, :admin, :employee, :created_at, :updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 TascPortal::Application.routes.draw do
   devise_for :users
+
+  scope '/admin' do
+    resources :users
+  end
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
