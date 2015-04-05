be7fa17b2785e61e89e52f3cc21acf22a6f0bc92
Morgan Bird <morgan.bird@gmail.com>
2015-04-05 12:02:21 -0500

    Commented out all reinvent-related models. :

--- app/models/re_resume.rb
+++ app/models/re_resume.rb
@@ -1,11 +1,11 @@
-class ReResume < ActiveRecord::Base
-  establish_connection :reinvent
-  self.table_name = 'JobsDb_Resumes'
-  self.primary_key = 'ResumeID'
-
-  belongs_to :re_education_level, :foreign_key => 'EducationLevelID'
-  belongs_to :re_education_level2, :foreign_key => 'EducationLevelID2'
-  belongs_to :re_education_level3, :foreign_key => 'EducationLevelID3'
-  belongs_to :re_experience_level, :foreign_key => 'ExperienceLevel'
-  has_many :re_my_expertises, :foreign_key => 'ResumeID'
-end
+# class ReResume < ActiveRecord::Base
+#   establish_connection :reinvent
+#   self.table_name = 'JobsDb_Resumes'
+#   self.primary_key = 'ResumeID'
+#
+#   belongs_to :re_education_level, :foreign_key => 'EducationLevelID'
+#   belongs_to :re_education_level2, :foreign_key => 'EducationLevelID2'
+#   belongs_to :re_education_level3, :foreign_key => 'EducationLevelID3'
+#   belongs_to :re_experience_level, :foreign_key => 'ExperienceLevel'
+#   has_many :re_my_expertises, :foreign_key => 'ResumeID'
+# end
